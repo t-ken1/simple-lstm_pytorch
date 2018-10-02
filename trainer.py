cc40d95946ac6828f915d398c0a922091cfb6675
t-ken1 <ken1@dhcp109.lab>
2018-10-02 16:25:51 +0900

    Fix trainer.py

--- trainer.py
+++ trainer.py
@@ -9,7 +9,8 @@ class Trainer(object):
         self.criterion = criterion
         self.optimizer = optimizer
 
-    def _make_batch(self, data, batch_size=100):
+
+    def make_batch(self, data, batch_size=100):
         N, D, T = data.shape
         num_batch = N // batch_size
         if N % batch_size != 0:
@@ -22,11 +23,11 @@ class Trainer(object):
         return batches
 
     def train(self, max_epoch, batch_size=100):
+        x_batches = self.make_batch(self.xs, batch_size=batch_size)
+        y_batches = self.make_batch(self.ys, batch_size=batch_size)
+        
         for epoch in range(max_epoch):
             running_loss = 0.0
-
-            x_batches = self._make_batch(self.xs, batch_size=batch_size)
-            y_batches = self._make_batch(self.ys, batch_size=batch_size)
 
             for x_batch, y_batch in zip(x_batches, y_batches):
                 # torch.tensor形式への変換
