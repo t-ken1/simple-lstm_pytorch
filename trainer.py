fba7bd0de1133c14c48dc6241e386ee6bedc1299
t-ken1 <ken1@dhcp109.lab>
2018-10-02 14:02:42 +0900

    Add trainer.py

---
+++ trainer.py
@@ -0,0 +1,55 @@
+import torch
+
+
+class Trainer(object):
+    def __init__(self, xs, ys, model, criterion, optimizer):
+        self.xs = xs
+        self.ys = ys
+        self.model = model
+        self.criterion = criterion
+        self.optimizer = optimizer
+
+    def _make_batch(self, data, batch_size=100):
+        N, D, T = data.shape
+        num_batch = N // batch_size
+        if N % batch_size != 0:
+            num_batch += 1
+
+        batches = []
+        for i in range(num_batch):
+            idx = i * batch_size
+            batches.append(data[idx:idx+batch_size, :])
+        return batches
+
+    def train(self, max_epoch, batch_size=100):
+        for epoch in range(max_epoch):
+            running_loss = 0.0
+
+            x_batches = _make_batch(xs, batch_size=batch_size)
+            y_batches = _make_batch(ys, batch_size=batch_size)
+
+            for x_batch, y_batch in zip(x_batches, y_batches):
+                # torch.tensor形式への変換
+                x = torch.tensor(x_batch, dtype=torch.float)
+                y = torch.tensor(y_batch, dtype=torch.float)
+
+                # lstmへの入力は (N, T, D) だが、現在は (N, D, T) なので
+                # 軸を入れ替える
+                x = x.transpose(1, 2)
+                y = y.transpose(1, 2)
+
+                # optimizerの初期化
+                optimizer.zero_grad()
+
+                # モデルに入力を入れて誤差の計算
+                output = model(x)
+                loss = criterion(output, y)
+
+                # 誤差を元に逆伝播を行い、パラメータを更新
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.item()
+
+                if epoch % 20 == 0:
+                    print('epoch: %d | loss: %.3f' % (epoch, running_loss))
